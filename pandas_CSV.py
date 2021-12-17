4e12e6af45e0a44371c77a7441da44be5f2506cd
caixin <2463089505@qq.com>
2021-12-17 19:23:26 +0800

    初始化

---
+++ pandas_CSV.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+'''
+# 读取数据并赋予列名
+df = pd.read_csv('csv_data1.txt', names=['population', 'profit', 'area'])
+print(df)
+'''
+
+'''
+# 将.txt 文件转换为 .csv 文件
+df.to_csv('house_price.csv')
+'''
