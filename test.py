7f3ed3bb1adb451437efb81b06c52208a8e809b2
pumpbumb <2463089505@qq.com>
2022-03-23 15:10:18 +0800

    提交test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 
 x1 = pd.Series([1, 2, 3, 4])
 x2 = pd.Series(data=[1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
-print(x1)
-print(x2)
+
+# 也可采用字典的方式来创建 Series
+d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+x3 = pd.Series(d, name='我是由字典创建的~')
+
+print(x1, '\n')
+print(x2, '\n')
+print(x3)
