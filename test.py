2288e4a4831752c3dfb30c55ea53ec848d673bea
pumpbumb <2463089505@qq.com>
2022-03-28 15:23:20 +0800

    修改test.py

--- test.py
+++ test.py
@@ -5,9 +5,11 @@ x1 = pd.Series([1, 2, 3, 4])
 x2 = pd.Series(data=[1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
 
 # 也可采用字典的方式来创建 Series
-d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+d = {1: 1, 666: 2, 'c': 3, 'd': 4}
 x3 = pd.Series(d, name='我是由字典创建的~')
 
 print(x1, '\n')
 print(x2, '\n')
-print(x3)
+
+print('-'*30)
+print('x3: ', type(x3), x3, x3.index, sep='\n\n')
