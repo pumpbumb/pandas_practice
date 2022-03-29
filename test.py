bd323736c616a8933465ca2a5b1d836bbd256546
pumpbumb <2463089505@qq.com>
2022-03-29 20:07:55 +0800

    修改test.py

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
+import numpy as np
 import pandas as pd
 
-x1 = pd.Series([1, 2, 3, 4])
-x2 = pd.Series(data=[1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+ser = pd.Series([1, 2, 3])
 
-# 也可采用字典的方式来创建 Series
-d = {1: 1, 666: 2, 'c': 3, 'd': 4}
-x3 = pd.Series(d, name='我是由字典创建的~')
+idx = pd.Index([4, 5, 6])
+print(type(idx))
 
-print(x1, '\n')
-print(x2, '\n')
+ser = np.maximum(ser, idx)
+print(type(ser), ser, sep='\n')
 
-print('-'*30)
-print('x3: ', type(x3), x3, x3.index, sep='\n\n')
+print(pd.DataFrame(np.random.randn(3, 12)))
